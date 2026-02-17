ae044a772bcde7c1443ccfcd1d8a57c8108ae86c
rs626s <61384559+rs626s@users.noreply.github.com>
2026-02-16 19:35:18 -0600

    Add files via upload

---
+++ src/test/java/edu/missouristate/csc615/chatbot/HrChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.missouristate.csc615.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
