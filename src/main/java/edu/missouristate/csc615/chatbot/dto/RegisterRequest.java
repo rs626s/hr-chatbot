f6b083aa2a90686fde86b3914018845f31f71765
rs626s <61384559+rs626s@users.noreply.github.com>
2026-02-16 19:19:42 -0600

    Add files via upload

--- src/main/java/edu/missouristate/csc615/chatbot/dto/RegisterRequest.java
+++ src/main/java/edu/missouristate/csc615/chatbot/dto/RegisterRequest.java
@@ -9,14 +9,17 @@ import lombok.Data;
 public class RegisterRequest {
 
     @NotBlank(message = "Username is required")
-    @Size(min = 3, max = 50)
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
 
     @NotBlank(message = "Email is required")
-    @Email(message = "Email should be valid")
+    @Email(message = "Email must be valid")
     private String email;
 
     @NotBlank(message = "Password is required")
-    @Size(min = 6, max = 100)
+    @Size(min = 6, max = 100, message = "Password must be between 6 and 100 characters")
     private String password;
+
+    private String firstName;
+    private String lastName;
 }
