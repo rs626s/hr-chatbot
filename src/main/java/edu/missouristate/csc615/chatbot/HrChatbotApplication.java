26ce113aaab4a0b81c4cb0e58104333056da5edc
rs626s <61384559+rs626s@users.noreply.github.com>
2026-02-16 19:36:11 -0600

    Add files via upload

---
+++ src/main/java/edu/missouristate/csc615/chatbot/HrChatbotApplication.java
@@ -0,0 +1,13 @@
+package edu.missouristate.csc615.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrChatbotApplication.class, args);
+    }
+
+}
