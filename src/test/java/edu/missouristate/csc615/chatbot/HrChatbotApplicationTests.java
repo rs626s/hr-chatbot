0ff808f4a0a8b1bfd282514ef8b68a1be90f97d9
rs626s <61384559+rs626s@users.noreply.github.com>
2026-02-21 23:13:47 -0600

    Add files via upload

--- src/test/java/edu/missouristate/csc615/chatbot/HrChatbotApplicationTests.java
+++ src/test/java/edu/missouristate/csc615/chatbot/HrChatbotApplicationTests.java
@@ -4,8 +4,18 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+
 @ActiveProfiles("test")
+@SpringBootTest(
+        properties = {
+                "spring.datasource.url=jdbc:h2:mem:testdb",
+                "spring.datasource.driver-class-name=org.h2.Driver",
+                "spring.datasource.username=sa",
+                "spring.datasource.password=",
+                "spring.jpa.hibernate.ddl-auto=create-drop",
+                "spring.flyway.enabled=false"
+        }
+)
 class HrChatbotApplicationTests {
 
     @Test
