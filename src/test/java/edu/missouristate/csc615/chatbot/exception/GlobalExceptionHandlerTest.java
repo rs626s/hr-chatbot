4198e5bedce58c0acb14c8b873050a430f9b5089
rs626s <61384559+rs626s@users.noreply.github.com>
2026-02-21 23:18:22 -0600

    Add files via upload

--- src/test/java/edu/missouristate/csc615/chatbot/exception/GlobalExceptionHandlerTest.java
+++ src/test/java/edu/missouristate/csc615/chatbot/exception/GlobalExceptionHandlerTest.java
@@ -14,9 +14,16 @@ import org.springframework.web.bind.annotation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
-@ActiveProfiles("test")
+@SpringBootTest(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.flyway.enabled=false"
+})
 class GlobalExceptionHandlerTest {
 
     @Autowired
