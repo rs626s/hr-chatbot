0c6a864010231510305f46dd5b937d2a9bdc0252
rs626s <61384559+rs626s@users.noreply.github.com>
2026-02-21 23:27:11 -0600

    Add files via upload

--- src/test/java/edu/missouristate/csc615/chatbot/HrChatbotApplicationTests.java
+++ src/test/java/edu/missouristate/csc615/chatbot/HrChatbotApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
         properties = {
-                "spring.datasource.url=jdbc:h2:mem:testdb",
+                "spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL",
                 "spring.datasource.driver-class-name=org.h2.Driver",
                 "spring.datasource.username=sa",
                 "spring.datasource.password=",
