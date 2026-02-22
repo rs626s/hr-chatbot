3c44250d486dcef38e457dad112c15178a3f29dd
rs626s <61384559+rs626s@users.noreply.github.com>
2026-02-21 23:20:20 -0600

    Add files via upload

--- src/test/java/edu/missouristate/csc615/chatbot/HrChatbotApplicationTests.java
+++ src/test/java/edu/missouristate/csc615/chatbot/HrChatbotApplicationTests.java
@@ -2,10 +2,8 @@ package edu.missouristate.csc615.chatbot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 
-@ActiveProfiles("test")
 @SpringBootTest(
         properties = {
                 "spring.datasource.url=jdbc:h2:mem:testdb",
